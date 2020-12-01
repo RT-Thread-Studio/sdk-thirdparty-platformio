0ee845897b42304aa19f0f76d02430fec15cec73
yaxing.chen <chenyaxing@rt-thread.com>
2020-12-01 15:13:33 +0800

    resize package

--- post_install.bat
+++ post_install.bat
@@ -2,5 +2,5 @@
 rem: Note %~dp0 get path of this batch file
 set current_dir=%~dp0
 set current_drive=%~d0
-
+echo The platformIO installation process is being executed, it may take more than 10 minutes, please be patient...
 %current_dir%python377x64/python.exe %current_dir%script.py %*
